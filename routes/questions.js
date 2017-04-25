24c987db4a9972dfbde6931284d9314f832a3ce5
Chase Sang <chase.sang@gmail.com>
2017-04-25 16:04:34 -0700

    added views

--- routes/questions.js
+++ routes/questions.js
@@ -9,7 +9,12 @@ router.get('/', function (request, response, next) {
   Question
     .findAll() // this returns promise...
     .then(function (questions) { // allowing us to use .then
-      response.send(questions);
+      // the path of the template that response.render takes
+      // is relative to the view/ folder by default
+      response.render('questions/index', {questions: questions});
+      // the second argument passed to response.render is an
+      // object where all its properties will be available to
+      // the rendered template as variables
     });
   // All Sequelize query methods return a promise
 })
